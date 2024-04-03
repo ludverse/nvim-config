4367b776e3601cdbd5ddf688d216cca21ce0156e
ludverse <62475903+ludverse@users.noreply.github.com>
2024-04-03 10:19:02 +0200

    set rust-analzyer to use check all features

--- lua/lsps.lua
+++ lua/lsps.lua
@@ -1,6 +1,14 @@
 return {
     ["lua_ls"] = {},
-    ["rust_analyzer"] = {},
+    ["rust_analyzer"] = {
+        settings = {
+            ["rust-analyzer"] = {
+                cargo = {
+                    features = "all"
+                }
+            }
+        }
+    },
     ["tsserver"] = {},
     ["html"] = {
         filetypes = { "vue", "html", "xml", "svg" }
